1b68815968ebc2486e60ac377743a6d5076c3267
Christian <christiandouglas57@gmail.com>
2024-04-09 17:33:13 -0500

    created my first Flask app

--- app/config.py
+++ app/config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config(object):
     """Base Config Object"""
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    MAIL_SERVER="sandbox.smtp.mailtrap.io"
+    MAIL_PORT=465
+    MAIL_USERNAME="e76c3e3a935dc0"
+    MAIL_PASSWORD="96d4a62b4242cd"
+    SECRET_KEY="runguyrun"
